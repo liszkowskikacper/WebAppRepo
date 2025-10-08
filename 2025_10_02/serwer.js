33fcd570f9e46a839f2482a6306b9eafe6a8ccaa
liszkowskikacper <144985782+liszkowskikacper@users.noreply.github.com>
2025-10-08 23:26:36 +0200

    Add files via upload

---
+++ 2025_10_02/serwer.js
@@ -0,0 +1,38 @@
+let http = require('http');
+const url = require('url');
+let fs = require('fs/promises');
+http.createServer(async (req, res)=> {
+    let path = req.url;
+    switch(path){
+        case "/":
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end("Strona główna");
+            break;
+        case "/jason":
+            let jason = {Grade: "Jedynka", Mood: "Sad"}
+            let tekst = JSON.stringify(jason);
+            res.end(tekst);
+            break;
+        case "/plik":
+            res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+            const file = await fs.readFile('plik.html', 'utf8')
+            res.end(file.toString())
+            break
+        case "/html":
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end("<h1>hello</h1><h6>WORLD</h6>")
+            break;
+        case "/get_params":
+            const parsedUrl = url.parse(path, true);
+            if (parsedUrl.pathname === '/get_params' && req.method === 'GET') {
+            console.log('Odebrane parametry GET:', parsedUrl.query);
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Parametry zostały odebrane. Sprawdź konsolę.');
+            }
+            break;
+        default:
+            res.status = 404
+            res.end('Error: Not Found!')
+    }
+}).listen(8080);
+
